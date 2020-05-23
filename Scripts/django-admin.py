925dc0a8d6f466ce125fbde43a4f9230456958ee
miketpl <mike@thepromisedlan.co.uk>
2020-05-23 16:20:14 +0100

    initial

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\0100110101001001\documents\python projects\venv\todo_web\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
